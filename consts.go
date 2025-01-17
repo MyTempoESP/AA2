4c5560b0c4c4329539f29bc961e1ddca2e8f45ad
TsukiGva2 <tsukigva@gmail.com>
2025-01-17 12:41:45 -0300

    AGEJAIFFDD

---
+++ consts.go
@@ -0,0 +1,12 @@
+package main
+
+const (
+	ANTENNA_QUEUE         = "antenna_envio_analytics"
+	ANTENNA_ROUTE         = "antenna.#"
+	ANTENNA_EXCHANGE      = "antenna_exchange"
+	ANTENNA_CONSUMER_NAME = "antenna_consumer_analytics"
+
+	NAO_ENVIADOS_QUEUE    = "nao_enviados"
+	NAO_ENVIADOS_ROUTE    = "nao_enviados"
+	NAO_ENVIADOS_EXCHANGE = "nao_enviados"
+)
