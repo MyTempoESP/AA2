afc140158811d93ab9a8cd542c2d5c7e9fdcd914
TsukiGva2 <tsukigva@gmail.com>
2025-01-23 16:02:18 -0300

    [fix] 🐛: added error check in CopyToUSB

--- action.go
+++ action.go
@@ -22,7 +22,7 @@ func CopyToUSB(device *usb.Device, file *file.File) (err error) {
 
 	now := time.Now()
 
-	file.Upload(fmt.Sprintf("/mnt/MYTEMPO-%02d_%02d_%02d", now.Hour(), now.Minute(), now.Second()))
+	err = file.Upload(fmt.Sprintf("/mnt/MYTEMPO-%02d_%02d_%02d", now.Hour(), now.Minute(), now.Second()))
 
 	return
 }
